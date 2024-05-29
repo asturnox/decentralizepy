2749e40cc82c0f81ff68268561eebfd6ab69280e
Santiago <94397934+asturnox@users.noreply.github.com>
2024-05-29 21:22:02 +0200

    add low mobility and high mobility experiments

--- _submit/batch.sh
+++ _submit/batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=compute
 #SBATCH --time=4:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=4G
 
-srun deploy.sh $1
+srun deploy.sh
