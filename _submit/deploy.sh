b5239f7185da53c425899831b8e0cf7e2ef0d949
Santiago <94397934+asturnox@users.noreply.github.com>
2024-05-30 13:51:56 +0200

    prepare 0.05 experiment

--- _submit/deploy.sh
+++ _submit/deploy.sh
@@ -7,9 +7,10 @@ cd tutorial/
 iterations=1000
 test_after=10
 procs_per_machine=48
-# ./run_decentralized.sh dynamic_48_0_0.txt $procs_per_machine $iterations $test_after
+./run_decentralized.sh dynamic_48_0_0.txt $procs_per_machine $iterations $test_after
+./run_decentralized.sh dynamic_48_0_05.txt $procs_per_machine $iterations $test_after
 ./run_decentralized.sh dynamic_48_0_2.txt $procs_per_machine $iterations $test_after
 ./run_decentralized.sh dynamic_48_0_4.txt $procs_per_machine $iterations $test_after
-./run_decentralized.sh dynamic_48_0_6.txt $procs_per_machine $iterations $test_after
-./run_decentralized.sh dynamic_48_0_8.txt $procs_per_machine $iterations $test_after
-# ./run_decentralized.sh dynamic_48_1_0.txt $procs_per_machine $iterations $test_after
+# ./run_decentralized.sh dynamic_48_0_6.txt $procs_per_machine $iterations $test_after
+# ./run_decentralized.sh dynamic_48_0_8.txt $procs_per_machine $iterations $test_after
+./run_decentralized.sh dynamic_48_1_0.txt $procs_per_machine $iterations $test_after
