cd6bb7fff494b8cf26ed374b3d8c7d33357894a9
Santiago <94397934+asturnox@users.noreply.github.com>
2024-05-30 16:04:56 +0200

    fix transfer

--- _submit/transfer.sh
+++ _submit/transfer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-(cd $SCRIPTPATH/../ &&  rsync -azv -e 'ssh -A -J sdeheredia@student-linux.tudelft.nl' --exclude='eval/' --exclude='**/decentralizepy.egg-info' --exclude='**/__pycache__' --exclude='.venv' [!.]* sdeheredia@login.delftblue.tudelft.nl:./decentralizepy && cd _submit)
+(cd $SCRIPTPATH/../ &&  rsync -azv -e 'ssh -A -J sdeheredia@student-linux.tudelft.nl' --exclude='eval/' --exclude='**/decentralizepy.egg-info' --exclude='**/__pycache__' --exclude='.venv' [!.]* sdeheredia@login.delftblue.tudelft.nl:/scratch/sdeheredia/small/decentralizepy && cd _submit)
