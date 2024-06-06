1d573faa3ed26db31d8c7f368974ce47f6c437d8
Santiago <94397934+asturnox@users.noreply.github.com>
2024-06-06 16:25:10 +0200

    wip: save first diff high low analsyis

--- _submit/transfer_out.sh
+++ _submit/transfer_out.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-IP=34.76.40.202
+IP=34.22.224.111
 (cd $SCRIPTPATH/../ &&  rsync -azv --stats -P  --exclude='**/__pycache__' --exclude='**/*.pt' --exclude='**/*.png' --exclude='.venv' --exclude='**/cifar*' --exclude='**/*.log' $IP:~/decentralizepy/eval/data/ ./.saved_experiments && cd _submit)
