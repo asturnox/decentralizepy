b5bcdbfca01c01c9f6715059ac74c7dab050bee8
Santiago <94397934+asturnox@users.noreply.github.com>
2024-06-08 02:57:17 +0200

    prepare plain niid batch

--- _submit/transfer.sh
+++ _submit/transfer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-IP=35.187.18.139
+IP=34.38.90.65
 (cd $SCRIPTPATH/../ &&  rsync -azv --progress --stats --exclude='tutorial/dynamic*' --exclude='**/2024*' --exclude='**/decentralizepy.egg-info' --exclude='**/__pycache__' --exclude='.venv' [!.]* $IP:~/decentralizepy && cd _submit)
