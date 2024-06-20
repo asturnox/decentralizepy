ca4812264130ecbbde9f99e0d7a9c08d5bb007e8
Santiago <94397934+asturnox@users.noreply.github.com>
2024-06-20 13:36:23 +0200

    prep batch of iid sol experiments

--- _submit/deploy.sh
+++ _submit/deploy.sh
@@ -7,7 +7,7 @@ source .venv/bin/activate
 cd tutorial/
 
 iterations=4000
-test_after=400
+test_after=40
 procs_per_machine=48
 
 generate_graphs() {
@@ -25,6 +25,10 @@ run_experiment() {
 
     alpha=$1
     sed -i'' -e "s/^alpha=.*/alpha=${alpha}/" config.ini
+
+    current_time=$(date +"%Y-%m-%dT%H:%M")
+    mkdir "../eval/data/${current_time}-${alpha}"
+
     sync
 
     ./run_decentralized.sh dynamic_48_0_0.txt $procs_per_machine $iterations $test_after
@@ -36,15 +40,18 @@ run_experiment() {
     ./run_decentralized.sh dynamic_48_1_0.txt $procs_per_machine $iterations $test_after
 }
 
-run_experiment 0.1
+run_experiments() {
+    run_experiment 0.1
+    # run_experiment 0.2
+    run_experiment 0.3
+    # run_experiment 0.4
+}
 
-run_experiment 0.2
+run_experiments
 
-run_experiment 0.4
+run_experiments
 
-run_experiment 0.6
-
-run_experiment 0.8
+run_experiments
 
 # Shutdown
 sudo shutdown -h now
