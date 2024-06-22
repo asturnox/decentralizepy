0eaa35e4bb82781c6e9784ac16c8e9be9a77631c
Santiago <94397934+asturnox@users.noreply.github.com>
2024-06-22 19:46:16 +0200

    add final sol niid experiments

--- _submit/3transfer_out.sh
+++ _submit/3transfer_out.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-IP=34.140.239.112
+IP=34.77.242.220
 (cd $SCRIPTPATH/../ &&  rsync -azv --stats -P  --exclude='**/__pycache__' --exclude='**/*.pt' --exclude='**/*.png' --exclude='.venv' --exclude='**/cifar*' --exclude='**/*.log' $IP:~/decentralizepy/eval/data/ ./.saved_experiments && cd _submit)
